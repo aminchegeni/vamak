c7f272c615b2d7aaef9307c53e36498856c08292
aminchegeni <a_chegeni@isc.co.ir>
2024-08-02 23:20:17 +0330

    test: add some member records to database for test

---
+++ core/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO VAMAK.MEMBER (name, family, username, password, mobile, birthday) VALUES ('Amin', 'Chegeni', 'a_chegeni', /* plain: 1234 */'$2a$10$jQ72cpMhgCRDCGY0gC3NFOvlRYGkLC90TMbHVzLvDaoE7vV6GiKwu', '09163412114', '1988-05-21');
+INSERT INTO VAMAK.MEMBER (name, family, username, password, mobile, birthday) VALUES ('yasin', 'Chegeni', 'yasin', '{noop}1234', '09942250177', '2014-12-09');
+INSERT INTO VAMAK.MEMBER (name, family, username, password, mobile, birthday) VALUES ('yasna', 'Chegeni', 'yasna', /* plain: 1234 */'{MD5}81dc9bdb52d04dc20036dbd8313ed055', '09942250177', '2021-11-17');
