527cbd9625e50da82409c8daff72f69f950f1a77
aminchegeni <a_chegeni@isc.co.ir>
2024-08-04 23:07:58 +0330

    refactor: move business to service

--- core/src/main/java/ir/snapp/pay/side/project/vamak/core/api/impl/MemberController.java
+++ core/src/main/java/ir/snapp/pay/side/project/vamak/core/api/impl/MemberController.java
@@ -1,31 +1,21 @@
 package ir.snapp.pay.side.project.vamak.core.api.impl;
 
 import ir.snapp.pay.side.project.vamak.commons.dto.UserInfo;
-import ir.snapp.pay.side.project.vamak.commons.model.Member;
 import ir.snapp.pay.side.project.vamak.core.api.MemberMgmt;
-import ir.snapp.pay.side.project.vamak.core.repo.MemberRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import ir.snapp.pay.side.project.vamak.core.service.MemberService;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MemberController implements MemberMgmt {
 
-    @Autowired
-    private MemberRepository repo;
+    private final MemberService service;
 
-    @Autowired
-    private PasswordEncoder encoder;
+    public MemberController(MemberService service) {
+        this.service = service;
+    }
 
     @Override
     public void add(UserInfo userInfo) {
-        repo.save(Member.builder()
-                .name(userInfo.getName())
-                .family(userInfo.getFamily())
-                .username(userInfo.getUsername())
-                .password(encoder.encode(userInfo.getPassword()))
-                .mobile(userInfo.getMobile())
-                .birthday(userInfo.getBirthday())
-                .build());
+        service.createMember(userInfo);
     }
 }
--- core/src/main/java/ir/snapp/pay/side/project/vamak/core/service/MemberService.java
+++ core/src/main/java/ir/snapp/pay/side/project/vamak/core/service/MemberService.java
@@ -1,7 +1,10 @@
 package ir.snapp.pay.side.project.vamak.core.service;
 
 import ir.snapp.pay.side.project.vamak.commons.constraint.Unique;
+import ir.snapp.pay.side.project.vamak.commons.dto.UserInfo;
+import ir.snapp.pay.side.project.vamak.commons.model.Member;
 import ir.snapp.pay.side.project.vamak.core.repo.MemberRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service("checker.username")
@@ -9,12 +12,26 @@ public class MemberService implements Unique.Checker<String> {
 
     private final MemberRepository repo;
 
-    public MemberService(MemberRepository repo) {
+    private final PasswordEncoder encoder;
+
+    public MemberService(MemberRepository repo, PasswordEncoder encoder) {
         this.repo = repo;
+        this.encoder = encoder;
     }
 
     @Override
     public boolean isExist(String username) {
         return repo.existsByUsername(username);
+    }
+
+    public void createMember(UserInfo userInfo) {
+        repo.save(Member.builder()
+                .name(userInfo.getName())
+                .family(userInfo.getFamily())
+                .username(userInfo.getUsername())
+                .password(encoder.encode(userInfo.getPassword()))
+                .mobile(userInfo.getMobile())
+                .birthday(userInfo.getBirthday())
+                .build());
     }
 }
